bb5539bce7b52024c5e66ff29f384c2242cbb3f4
Charlie Harmon <wharmon@wisc.edu>
2026-04-12 21:12:49 -0500

    SortingUnit V0

---
+++ hardware/rtl/SortingUnit/SortingUnit.sv
@@ -0,0 +1,23 @@
+/*
+    Temporal Sorting Unit for Reformer Accelerator
+
+    Inputs:
+        -i_b_i: From ArgMax units, multiple bucket indicies to be sorted
+    Outputs:
+        -o_index: index of input vector
+        -o_bucket: what bucket the input vector has been sorted into
+        -o_valid: indicates if the other outputs are valid or not
+        
+*/
+
+`default_nettype none
+
+module SortingUnit(
+
+
+);
+
+
+
+
+endmodule
